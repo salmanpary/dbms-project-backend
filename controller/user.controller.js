f9875cc4a621318a8ac9afc6bddd5c1dee5d2c79
Sudeep S <123sudeepsangeeth@gmail.com>
2022-12-10 22:02:58 +0530

    Login set

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -19,8 +19,18 @@ const Signup=async (req,res) => {
     }
 
 const Login= async (req, res) => {
-    res.json({
-        login: "testing"
+    const {username,password}=req.body
+    await connection.query("SELECT * FROM `users` WHERE username=? AND password=? LIMIT 1",
+    [username,password],
+    function (err,result) {
+        if(err || !result) {
+            res.json({login:false});
+            throw err;
+        }
+        else{
+            res.json(result[0]);
+        }
+        console.log(result);
     })
 }
 module.exports = {Login,Signup}
--- routes/user.router.js
+++ routes/user.router.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
 const { Login,Signup } = require("../controller/user.controller");
 router.post("/signup",Signup);
-router.get("/login", Login);
+router.post("/login", Login);
 module.exports = router;
