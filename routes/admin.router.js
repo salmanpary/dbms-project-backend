0d5b9a5c28d8801da725aadfbfddf09035807721
Sudeep S <123sudeepsangeeth@gmail.com>
2022-12-12 15:00:15 +0530

    done:finish!1

--- routes/admin.router.js
+++ routes/admin.router.js
@@ -1,4 +1,7 @@
 const router=require('express').Router();
-const {AddProduct}=require('../controller/admin.controller');
+const {AddProduct, DisplayOrder,PaymentNotdone,PaymentDone}=require('../controller/admin.controller');
 router.post("/addproducts",AddProduct)
+router.get("/displayorder",DisplayOrder)
+router.get("/nopayment",PaymentNotdone)
+router.get("/paymentdone",PaymentDone)
 module.exports=router;
